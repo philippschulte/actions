2236b3ee4c39971d1894263428ec37569f77cf67
philippschulte <philipp.schulte@ymail.com>
2019-09-26 15:02:28 -0700

    refactor(staging): cleanup

--- tf/staging/main.tf
+++ tf/staging/main.tf
@@ -12,20 +12,19 @@ resource "fastly_service_v1" "api-service-staging-1SOF2sf218IFdLZhnqBlKB" {
     comment = "Staging"
   }
 
-  domain {
-    name    = "s2.boombumerang.com"
-    comment = "Staging"
-  }
-
   backend {
-    address = "1.2.3.4"
-    name    = "localhost"
-    port    = 80
-  }
-
-  backend {
-    address = "1.2.3.4"
-    name    = "localhost2"
-    port    = 80
+    address               = "1.2.3.4"
+    auto_loadbalance      = true
+    between_bytes_timeout = 10000
+    connect_timeout       = 1000
+    error_threshold       = 0
+    first_byte_timeout    = 15000
+    max_conn              = 200
+    ssl_check_cert        = true
+    use_ssl               = false
+    weight                = 100
+    name                  = "localhost"
+    port                  = 80
   }
 }
+
