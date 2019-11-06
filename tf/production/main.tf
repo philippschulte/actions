de5f19514a81fef85d2a64f28b22edde81efeb98
philippschulte <philipp.schulte@ymail.com>
2019-11-06 13:31:28 -0800

    refactor

--- tf/production/main.tf
+++ tf/production/main.tf
@@ -8,48 +8,12 @@ resource "fastly_service_v1" "api-service-production-0p7ElaomsexoNHwdMbYJac" {
   name     = "Production"
 
   domain {
-    name    = "www.action-demo-prod.com"
-    comment = "www"
+  name = "www.altitude-ny.com"
   }
 
   backend {
-    address               = "1.2.3.4"
-    auto_loadbalance      = true
-    between_bytes_timeout = 10000
-    connect_timeout       = 1000
-    error_threshold       = 0
-    first_byte_timeout    = 15000
-    max_conn              = 200
-    ssl_check_cert        = true
-    use_ssl               = false
-    weight                = 100
-    name                  = "localhost"
-    port                  = 80
+    address = "1.2.3.4"
+    name    = "localhost"
+    port    = 80
   }
-
-  gcslogging {
-    name               = join("", var.GCS_LOGGING_NAME)
-    email              = "test@gmail.com"
-    bucket_name        = "my_logs_bucket"
-    secret_key         = join("", var.GCS_LOGGING_SECRET_KEY)
-    path               = "production/"
-    period             = 3600
-    gzip_level         = 0
-    format             = "%h %l %u %t \"%r\" %>s %b"
-    timestamp_format   = "%Y-%m-%dT%H:%M:%S.000"
-    response_condition = ""
-    message_type       = "blank"
-  }
-}
-
-output "active_version" {
-  value = fastly_service_v1.api-service-production-0p7ElaomsexoNHwdMbYJac.active_version
-}
-
-output "cloned_version" {
-  value = fastly_service_v1.api-service-production-0p7ElaomsexoNHwdMbYJac.cloned_version
-}
-
-output "service_id" {
-  value = fastly_service_v1.api-service-production-0p7ElaomsexoNHwdMbYJac.id
 }
--- tf/staging/main.tf
+++ tf/staging/main.tf
@@ -8,34 +8,12 @@ resource "fastly_service_v1" "api-service-staging-1SOF2sf218IFdLZhnqBlKB" {
   name     = "Staging"
 
   domain {
-    name    = "www.action-demo-stag.com"
-    comment = "www"
+    name = "www.altitude-ny-stag.com"
   }
 
   backend {
-    address               = "1.2.3.4"
-    auto_loadbalance      = true
-    between_bytes_timeout = 10000
-    connect_timeout       = 1000
-    error_threshold       = 0
-    first_byte_timeout    = 15000
-    max_conn              = 200
-    ssl_check_cert        = true
-    use_ssl               = false
-    weight                = 100
-    name                  = "localhost"
-    port                  = 80
+    address = "1.2.3.4"
+    name    = "localhost"
+    port    = 80
   }
-}
-
-output "active_version" {
-  value = fastly_service_v1.api-service-staging-1SOF2sf218IFdLZhnqBlKB.active_version
-}
-
-output "cloned_version" {
-  value = fastly_service_v1.api-service-staging-1SOF2sf218IFdLZhnqBlKB.cloned_version
-}
-
-output "service_id" {
-  value = fastly_service_v1.api-service-staging-1SOF2sf218IFdLZhnqBlKB.id
 }
