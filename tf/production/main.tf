ad5bc9554def7720d306417b1b46e723bb1535c7
philippschulte <philipp.schulte@ymail.com>
2020-04-06 20:30:30 -0700

    improvements

--- tf/production/main.tf
+++ tf/production/main.tf
@@ -41,15 +41,3 @@ resource "fastly_service_v1" "api-service-production-0p7ElaomsexoNHwdMbYJac" {
     message_type       = "blank"
   }
 }
-
-output "active_version" {
-  value = fastly_service_v1.api-service-production-0p7ElaomsexoNHwdMbYJac.active_version
-}
-
-output "cloned_version" {
-  value = fastly_service_v1.api-service-production-0p7ElaomsexoNHwdMbYJac.cloned_version
-}
-
-output "service_id" {
-  value = fastly_service_v1.api-service-production-0p7ElaomsexoNHwdMbYJac.id
-}
